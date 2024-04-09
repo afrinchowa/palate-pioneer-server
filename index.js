480fb4134b3b00adff88c8ce27793065ae307a6c
Chowa <arfinchowa524@gmail.com>
2024-04-09 23:12:47 +0600

    middleware updated

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const cors = require('cors');
 
 // middleware
 app.use(cors());
+app.use(express.json());
+
+app.get('/',(req, res) =>{
+    res.send('palate is sitting')
+})
+app.listen(port, () => {
+    console.log(`Palate is sitting on port ${port}`);
+})
