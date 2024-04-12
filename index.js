0431a714e964639b54ebf577c0ce2050bc087ea2
Chowa <arfinchowa524@gmail.com>
2024-04-12 23:55:21 +0600

    port added

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/',(req, res) =>{
-    res.send('Palate pioneer is Running')
+    res.send('Palate pioneer is Running on the port')
 })
 app.listen(port, () => {
     console.log(`Palate is sitting on port ${port}`);
