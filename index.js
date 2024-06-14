c27e4f172b46e9fb6c0ed483d49535914698cf16
Chowa <arfinchowa524@gmail.com>
2024-06-14 23:05:11 +0600

    delete operation created

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-const express = require ( 'express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-require('dotenv').config()
-const port =process.env.PORT || 5000;
+const cors = require("cors");
+require("dotenv").config();
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.033buvy.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -18,29 +17,83 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const userCollection = client.db("palatedb").collection("users");
+    const menuCollection = client.db("palatedb").collection("menu");
+    const reviewCollection = client.db("palatedb").collection("reviews");
+    const cartCollection = client.db("palatedb").collection("carts");
+
+    // users related api
+    app.post('/users', async(req,res)=>{
+      const user =req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    } )
+
+// menu related api
+    app.get("/menu", async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+    // review related api
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result);
+    });
+    // carts collection
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.post("/carts", async (req, res) => {
+      const cartItem = req.body;
+      const result = await cartCollection.insertOne(cartItem);
+      res.send(result);
+    });
+app.delete("/carts/:id", async(req,res) =>{
+  const id =req.params.id;
+  const query={_id:new ObjectId(id)}
+  const result =await cartCollection.deleteOne(query)
+  res.send(result)
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/',(req, res) =>{
-    res.send('Palate pioneer is Running on the port')
-})
+app.get("/", (req, res) => {
+  res.send("Palate pioneer is Running on the port");
+});
 app.listen(port, () => {
-    console.log(`Palate is sitting on port ${port}`);
-})
+  console.log(`Palate is sitting on port ${port}`);
+});
+/**
+ * ------------------
+ * naming convention
+ * ------------------
+ * app.get ("/users")
+ * app.get ("/users/:id")
+ * app.post("/users")
+ * app.put("/users/:id")
+ * app.patch("/users/:id")
+ * app.delete("/users/:id")
+ */
