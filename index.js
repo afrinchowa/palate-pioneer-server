6df46ffa45ea2aa9a8b7d8b57d6a091f61bfdaa3
Chowa <arfinchowa524@gmail.com>
2024-06-15 23:44:48 +0600

    user collection setting up admin

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ async function run() {
     const cartCollection = client.db("palatedb").collection("carts");
 
     // users related api
+    app.get("users", async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result);
+    });
+
     app.post("/users", async (req, res) => {
       const user = req.body;
       // insert email if user dont exists
