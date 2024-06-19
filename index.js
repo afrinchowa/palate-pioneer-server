9525511e0ae52af7ec0deffcbaf2b0c5a105a7a7
Chowa <arfinchowa524@gmail.com>
2024-06-20 01:55:12 +0600

    admin authorization

--- index.js
+++ index.js
@@ -31,19 +31,54 @@ async function run() {
     const reviewCollection = client.db("palatedb").collection("reviews");
     const cartCollection = client.db("palatedb").collection("carts");
 
+// jwt related api
     app.post("/jwt", async (req, res) => {
       const user = req.body;
       const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        expiresIn: "1h"});
-        res.send({token})
+        expiresIn: "1h",
       });
-    
+      res.send({ token });
+    });
+
+// middlewares
+const verifyToken = (req,res,next) =>{
+
+  console.log('inside verify token',req.headers.authorization);
+  if(!req.headers.authorization){
+    return res.status(401).send({message:'forbidden access'})
+  }
+  const token = req.headers.authorization.split(' ')[1];
+jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+if(err){
+  return res.status(401).send({message:'forbidden access'})
+}
+req.decoded = decoded;
+next();
+})
+}
 
     // users related api
-    app.get("/users", async (req, res) => {
+    app.get("/users",verifyToken, async (req, res) => {
+      console.log(req.headers);
       const result = await userCollection.find().toArray();
       res.send(result);
     });
+
+app.get('/users/admin/:email', verifyToken, async(req,res) =>{
+  const email = req.params.email;
+  if(email !== req.decoded.email){
+    return res.status(403).send({message:'unauthorized access '})
+  }
+  const query={email:email};
+  const user = await userCollection.findOne(query);
+  let admin =false;
+  if(user){
+    admin =user.role=== 'admin';
+  }
+  res.send({admin});
+})
+
+
 
     app.post("/users", async (req, res) => {
       const user = req.body;
@@ -59,20 +94,18 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+      const result = await userCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    });
 
-app.patch('/users/admin/:id',async(req,res) => {
-  const id = req.params.id;
-  const filter = {_id: new ObjectId(id)};
-  const updatedDoc ={
-    $set :{
-      role:'admin'
-    }
-  }
-  const result = await userCollection.updateOne(filter,updatedDoc)
-  res.send(result);
-} )
-
-   
     app.delete("/users/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
