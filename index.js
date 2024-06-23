27d06c09c03fe20fa55256510de5bf9fe23e2b5b
Chowa <arfinchowa524@gmail.com>
2024-06-23 13:22:30 +0600

    updated Item

--- index.js
+++ index.js
@@ -127,12 +127,47 @@ next();
       res.send(result);
     });
 
+    app.get('/menu/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await menuCollection.findOne(query);
+      res.send(result);
+    });
+    
+
+
     app.post("/menu",verifyToken,verifyAdmin, async (req, res) => {
       const item = req.body;
       const result = await menuCollection.insertOne(item);
       res.send(result);
     });
 
+app.patch('/menu/:id',async(req,res) =>{
+  const item =req.body;
+  const id=req.params.id;
+  const filter ={_id:(id)}
+  const updatedDoc={
+    $set:{
+      name:item.name,
+      recipe:item.recipe,
+      category:item.category,
+      price:item.price,
+      image:item.image,
+
+    }
+  }
+
+  const result =await menuCollection.updateOne(filter,updatedDoc);
+  res.send(result);
+})
+
+
+    app.delete("/menu/:id",verifyToken,verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: (id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // review related api
     app.get("/reviews", async (req, res) => {
