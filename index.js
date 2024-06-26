5989a4ce0d7b8a0d1ee43515fb9e359d16a1e463
Chowa <arfinchowa524@gmail.com>
2024-06-27 00:13:49 +0600

    payment methods

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const stripe =require('stripe')(process.env.STRIPE_SECRET_KEY);
 const port = process.env.PORT || 4000;
+
+
 
 // middleware
 app.use(cors());
@@ -194,6 +197,24 @@ app.patch('/menu/:id',async(req,res) =>{
       const result = await cartCollection.deleteOne(query);
       res.send(result);
     });
+
+// payment intent
+app.post('/create-payment-intent',async (req,res) => {
+  const {price} =req.body;
+  const amount =parseInt(price*100)
+
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount:amount,
+    currency:'usd',
+    payment_method_types:['card']
+  });
+
+  res.send({
+    clientSecret:paymentIntent.client_secret
+  })
+})
+
+
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
