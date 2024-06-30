0fedae0da85229fa1239bfd45f2875d1e86eeb53
Chowa <arfinchowa524@gmail.com>
2024-06-30 23:36:37 +0600

    payment methods working up 3

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ async function run() {
     const menuCollection = client.db("palatedb").collection("menu");
     const reviewCollection = client.db("palatedb").collection("reviews");
     const cartCollection = client.db("palatedb").collection("carts");
+    const paymentCollection = client.db("palatedb").collection("payments");
 
     // jwt related api
     app.post("/jwt", async (req, res) => {
@@ -198,6 +199,8 @@ app.patch('/menu/:id',async(req,res) =>{
       res.send(result);
     });
 
+
+
 // payment intent
 app.post('/create-payment-intent',async (req,res) => {
   const {price} =req.body;
@@ -213,6 +216,19 @@ app.post('/create-payment-intent',async (req,res) => {
     clientSecret:paymentIntent.client_secret
   })
 })
+
+app.post('/payments', async(req,res) => {
+  const payment =req.body;
+  const paymentResult =await paymentCollection.insertOne(payment);
+  // carefully delete each item from the cart
+console.log('payment info', payment);
+const query = {_id:{
+  $in: payment.cartIds.map(id => new ObjectId(id))
+}};
+const deleteResult =await cartCollection.deleteMany(query);
+res.send({paymentResult, deleteResult})
+})
+
 
 
 
